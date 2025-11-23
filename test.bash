4de26c00a1a94c23f312c79ae1f09d0ca02f190d
RiMC7 <iinu04835@gmail.com>
2025-11-24 03:37:38 +0900

    Update files for submission

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash -xv 
 # SPDX-FileCopyrightText: 2025 Ryomu Inukai
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -10,12 +10,26 @@ ng () {
 res=0
 
 
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
+
+out=$(echo 5 | ./odd_even)
+[ "${out}" = "5 Odd" ] || ng "$LINENO"
+
+out=$(echo 10 | ./odd_even)
+[ "${out}" = "10 Even" ] || ng "$LINENO"
+
+out=$(echo あ | ./odd_even)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo 1.5 | ./odd_even)
+[ "$?" = 1 ] || ng "$LINENO"
+
+out=$(echo | ./odd_even)
+[ "$?" = 0 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 exit $res
-
 
 
 
