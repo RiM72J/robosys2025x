08fd95dbeba019988831060f101ede41558d30dc
RiMC7 <iinu04835@gmail.com>
2025-11-25 14:00:53 +0900

    change comand

--- test.bash
+++ test.bash
@@ -1,36 +1,30 @@
-#!/bin/bash -xv 
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2025 Ryomu Inukai
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-	echo ${1}行目が違うよ
-	res=1
+    echo ${1}行目が違うよ
+    res=1
 }
 
 res=0
 
+out=$(echo 12 | ./factorize)
+[ "${out}" = "12: 2 2 3" ] || ng "$LINENO"
 
+out=$(echo 13 | ./factorize)
+[ "${out}" = "13: 13" ] || ng "$LINENO"
 
-out=$(echo 5 | ./odd_even)
-[ "${out}" = "5 Odd" ] || ng "$LINENO"
+out=$(echo 100 | ./factorize)
+[ "${out}" = "100: 2 2 5 5" ] || ng "$LINENO"
 
-out=$(echo 10 | ./odd_even)
-[ "${out}" = "10 Even" ] || ng "$LINENO"
-
-out=$(echo あ | ./odd_even)
+out=$(echo あ | ./factorize 2>&1)
 [ "$?" = 1 ] || ng "$LINENO"
-[ "${out}" = "" ] || ng "$LINENO"
+echo ${out} | grep "Error" || ng "$LINENO"
 
-out=$(echo 1.5 | ./odd_even)
-[ "$?" = 1 ] || ng "$LINENO"
-
-out=$(echo | ./odd_even)
+out=$(echo | ./factorize)
 [ "$?" = 0 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 exit $res
-
-
-
-
